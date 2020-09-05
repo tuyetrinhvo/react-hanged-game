03c06d8fbff50ec5633e067b75e16bbfb053a666
Tuyetrinh VO <tuyetrinhvo@gmail.com>
2020-09-05 13:15:44 +0200

    Updates

---
+++ precache-manifest.6ce528087732de5b41b0c38806654b70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80dd3053181ecd0ce0d442b6df6a8d0c",
+    "url": "/react-hanged-game/index.html"
+  },
+  {
+    "revision": "0d6c61384f3b1194fe09",
+    "url": "/react-hanged-game/static/css/main.4f30021a.chunk.css"
+  },
+  {
+    "revision": "e285be3a4907ee252a8b",
+    "url": "/react-hanged-game/static/js/2.7dce7141.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-hanged-game/static/js/2.7dce7141.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d6c61384f3b1194fe09",
+    "url": "/react-hanged-game/static/js/main.c284b46d.chunk.js"
+  },
+  {
+    "revision": "c92f3136278d1e7babd7",
+    "url": "/react-hanged-game/static/js/runtime-main.e2bbb5b7.js"
+  }
+]);
